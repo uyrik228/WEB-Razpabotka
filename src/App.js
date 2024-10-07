6aada1b5e36d50d18d4a29a52b36377a8112ba13
uyrik228 <yuras.smol@mail.ru>
2024-10-07 11:06:59 +0300

    Login in App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import EmployeeAPI from "./api/service";
 import EmployeeTable from "./EmployeeTable";
 import Form from "./Form";
+import Login from "./Login";
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 const initialEmployees = EmployeeAPI.all();
 
 function App() {
   const [employees, setEmployees] = useState(initialEmployees);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const delEmp = (id) => {
     if (EmployeeAPI.delete(id)) {
@@ -23,10 +26,23 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Form handleSubmit={addEmployee} inEmployee={{ name: "", surname: "", age: "" }} />
-      <EmployeeTable employees={employees} delEmployee={delEmp} />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login setAuth={setIsAuthenticated} />} />
+          <Route path="/" element={
+            isAuthenticated ? (
+              <>
+                <Form handleSubmit={addEmployee} inEmployee={{ name: "", surname: "", age: "" }} />
+                <EmployeeTable employees={employees} delEmployee={delEmp} />
+              </>
+            ) : (
+              <Navigate to="/login" />
+            )
+          } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
